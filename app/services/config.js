61dfc536b5ab34b558134dfdf7a87b2b6913bb7e
okkim21 <okkim@Oks-MacBook-Pro.home>
2016-11-07 20:43:50 -0800

    scss

--- app/services/config.js
+++ app/services/config.js
@@ -12,28 +12,28 @@
         toastr.options.positionClass = 'toast-bottom-right';
     }
 
-    var config = {
-        appErrorPrefix: '[ChurchOffice-Modular Error] ', //Configure the exceptionHandler decorator
-        appTitle: 'ChurchOffice',
-        version: '1.0.0'
-    };
-
-    core.value('config', config);
-
-    core.config(configure);
-
-    configure.inject=['$logProvider', 'routerHelperProvider', 'exceptionHandlerProvider'];
-
-    function configure($logProvider, routerHelperProvider, exceptionHandlerProvider){
-
-        if($logProvider.debugEnabled()){
-            $logProvider.debugEnabled(true);
-        }
-
-        exceptionHandlerProvider.configure(config.appErrorPrefix);
-        routerHelperProvider.configure({docTitle: config.appTitle + ': '});
-
-    }
+    //var config = {
+    //    appErrorPrefix: '[ChurchOffice-Modular Error] ', //Configure the exceptionHandler decorator
+    //    appTitle: 'ChurchOffice',
+    //    version: '1.0.0'
+    //};
+    //
+    //core.value('config', config);
+    //
+    //core.config(configure);
+    //
+    //configure.inject=['$logProvider', 'routerHelperProvider', 'exceptionHandlerProvider'];
+    //
+    //function configure($logProvider, routerHelperProvider, exceptionHandlerProvider){
+    //
+    //    if($logProvider.debugEnabled()){
+    //        $logProvider.debugEnabled(true);
+    //    }
+    //
+    //    exceptionHandlerProvider.configure(config.appErrorPrefix);
+    //    routerHelperProvider.configure({docTitle: config.appTitle + ': '});
+    //
+    //}
 
 })();
 
