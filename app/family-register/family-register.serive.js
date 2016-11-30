0b378b6a46ff03065026bac97ab39950d9afea69
okkim21 <okkim@Oks-MacBook-Pro.home>
2016-11-30 10:40:11 -0800

    json

--- app/family-register/family-register.serive.js
+++ app/family-register/family-register.serive.js
@@ -8,8 +8,9 @@
     familyRegisterService.$inject =['$resource', '$q'];
 
     function familyRegisterService($resource, $q){
-        var basicUrl ='http://churchoffice-api.azurewebsites.net/api';
-        var resource = $resource(basicUrl + '/families/:id', { id: '@id' }, { 'update': { method: 'put' } });
+        //var basicUrl ='http://churchoffice-api.azurewebsites.net/api';
+        //var resource = $resource(basicUrl + '/families/:id', { id: '@id' }, { 'update': { method: 'put' } });
+        var resource = $resource('data/familyById.json');
         return {
             get: function (id) {
                 var deferred = $q.defer();
@@ -48,5 +49,46 @@
                 return deferred.promise;
             }
         }
+
+        //var basicUrl ='http://churchoffice-api.azurewebsites.net/api';
+        //var resource = $resource(basicUrl + '/families/:id', { id: '@id' }, { 'update': { method: 'put' } });
+        //return {
+        //    get: function (id) {
+        //        var deferred = $q.defer();
+        //        resource.get({id: id},
+        //            function (result) {
+        //                deferred.resolve(result);
+        //            },
+        //            function (response) {
+        //                deferred.reject(response);
+        //            }
+        //        );
+        //        return deferred.promise;
+        //    },
+        //    update: function (family, id) {
+        //        var deferred = $q.defer();
+        //        resource.update({id:id},family,
+        //            function (response) {
+        //                deferred.resolve(response);
+        //            },
+        //            function (response) {
+        //                deferred.reject(response);
+        //            }
+        //        );
+        //        return deferred.promise;
+        //    },
+        //    save: function(family) {
+        //        var deferred = $q.defer();
+        //        resource.save(family,
+        //            function(response) {
+        //                deferred.resolve(response);
+        //            },
+        //            function(response) {
+        //                deferred.reject(response);
+        //            }
+        //        );
+        //        return deferred.promise;
+        //    }
+        //}
     }
 })();
